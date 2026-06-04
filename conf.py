c63e0261991f4dacd784a4fd5f0592abbba22a1b
YourKEYEQ <you@gamil.com>
2026-06-04 11:09:46 +0800

    V08-2026-06-03

--- conf.py
+++ conf.py
@@ -26,7 +26,30 @@ author = 'keyestudio WiKi'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_copybutton"]
+extensions = ['myst_parser','sphinx_copybutton']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
 
 
 # Add any paths that contain templates here, relative to this directory.
